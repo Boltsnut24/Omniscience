b4a924b82a8d1378a7e9a4bc3022735c6db6f212
Brad <Brad@BRADSPC>
2021-02-15 15:12:32 -0700

    Getting my head around component structure

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,15 @@
+import Login from "./Login"
+import Signup from "./Signup"
+
+function Landing() {
+  return (
+    <>Homepage
+      <div>
+        <Login />
+        <Signup />
+      </div>
+    </>
+  );
+}
+
+export default Landing;
